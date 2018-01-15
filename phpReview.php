fa6bb3d8cb008da5e44ec9bfdd4fee5fbb28091a
Kevan Barter <Kbarter@mail.greenriver.edu>
2018-01-15 17:59:54 -0500

    second commit

--- phpReview.php
+++ phpReview.php
@@ -6,7 +6,22 @@
 <body>
     <h1>PHP Review</h1>
     <?php
+    //animals array
     $animals = array('panda', 'alpaca', 'boa');
+
+    //sorting animals array
+    function sortArray($animals)
+    {
+        sort($animals);
+        $length = count($animals);
+        for($i = 0; $i < $length; $i++)
+        {
+           echo $animals[$i].' ';
+        }
+
+    }
+
+    sortArray($animals);
     ?>
 </body>
 </html>
