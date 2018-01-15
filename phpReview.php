60648783b757ec124a72641063d5f20140a335ae
Kevan Barter <Kbarter@mail.greenriver.edu>
2018-01-15 17:27:01 -0500

    first commit

---
+++ phpReview.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Review</title>
+</head>
+<body>
+    <h1>PHP Review</h1>
+    <?php
+    $animals = array('panda', 'alpaca', 'boa');
+    ?>
+</body>
+</html>
